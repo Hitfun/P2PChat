2b1c8f4673969f28d57af25e854060492d8add93
Hitfun <peter.groenquist@gmail.com>
2017-12-17 21:57:20 +0100

    Back fixed
    (wifi disconnected after long inactivity? Minor issue and not important)

--- app/src/main/java/ch/ethz/inf/vs/a1/rubfisch/p2pchat/BroadcastReceiverActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a1/rubfisch/p2pchat/BroadcastReceiverActivity.java
@@ -326,7 +326,7 @@ public class BroadcastReceiverActivity extends AppCompatActivity implements Wifi
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1) {
+
             if (mManager != null && mChannel != null) {
                 mManager.requestGroupInfo(mChannel, new WifiP2pManager.GroupInfoListener() {
                     @Override
@@ -347,7 +347,7 @@ public class BroadcastReceiverActivity extends AppCompatActivity implements Wifi
                     }
                 });
             }
-        }
+
     }
 
 
